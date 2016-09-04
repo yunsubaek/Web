d5dd5dd306d158e61823c3db6aa36f43d03be878
yunsubaek <yunsubaek@naver.com>
2016-09-04 18:07:58 +0900

    16.09.04 생활코딩 수업
    16.09.03 내용과 중복된 부분 배제해야함.

---
+++ web/htdoc/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>웹수업</title>
+  </head>
+  <body>
+    <header>
+      <a href="index.html"><h1>생코웹</h1></a>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="1.html">html</a></li>
+        <li><a href="2.html">css</a></li>
+        <li><a href="3.html">javascript</a></li>
+      </ul>
+    </nav>
+    <article>
+      <!-- id에 따른 php를 삽입할 수 있다. -->
+      <!-- <?php
+        print(file_get_contents($_GET['id']));
+       ?> -->
+       <!-- DB를 사용할 경우 DB에 접속해서 입력값이 해당하는 정보를 가져오도록 구현. -->
+    </article>
+  </body>
+</html>
